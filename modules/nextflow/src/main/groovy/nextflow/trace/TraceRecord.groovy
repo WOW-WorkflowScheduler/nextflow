608e6555b9baeecd570d42435bb772e4b5b6c9da
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2022-05-05 11:11:08 +0200

    New values to trace
    Signed-off-by: Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>

--- modules/nextflow/src/main/groovy/nextflow/trace/TraceRecord.groovy
+++ modules/nextflow/src/main/groovy/nextflow/trace/TraceRecord.groovy
@@ -115,7 +115,11 @@ class TraceRecord implements Serializable {
             scheduler_tried_to_schedule: 'num',
             scheduler_init_runtime: 'num',
             scheduler_nodes_to_copy_from: 'num',
-            scheduler_time_to_schedule: 'num'
+            scheduler_time_to_schedule: 'num',
+            scheduler_nodes_tried: 'num',
+            scheduler_nodes_cost: 'str',
+            scheduler_could_stop_fetching: 'num',
+            scheduler_best_cost: 'num'
     ]
 
     static public Map<String,Closure<String>> FORMATTER = [
