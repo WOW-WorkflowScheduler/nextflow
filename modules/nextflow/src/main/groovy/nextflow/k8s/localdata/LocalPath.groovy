a2394d7f374f37b4d709eb9d96e2f0db5384c5ca
Friedrich Tschirpke <88798463+ftschirpke@users.noreply.github.com>
2022-06-28 17:02:55 +0200

    Optimize access patterns in LocalPath

--- modules/nextflow/src/main/groovy/nextflow/k8s/localdata/LocalPath.groovy
+++ modules/nextflow/src/main/groovy/nextflow/k8s/localdata/LocalPath.groovy
@@ -39,7 +39,7 @@ class LocalPath implements Path {
     }
 
     static LocalPath toLocalPath( Path path, LocalFileWalker.FileAttributes attributes, Path workDir ){
-        ( path instanceof  LocalPath ) ? path as LocalPath : new LocalPath( path, attributes, workDir )
+        ( path instanceof LocalPath ) ? path as LocalPath : new LocalPath( path, attributes, workDir )
     }
 
     static void setClient( K8sSchedulerClient client ){
@@ -52,7 +52,7 @@ class LocalPath implements Path {
         while ( true ) {
             try {
                 FtpClient ftpClient = FtpClient.create(daemon)
-                ftpClient.login("ftp", "nextflowClient".toCharArray() )
+                ftpClient.login("root", "password".toCharArray() )
                 ftpClient.enablePassiveMode( true )
                 return ftpClient
             } catch ( IOException e ) {
@@ -93,6 +93,17 @@ class LocalPath implements Path {
         response
     }
 
+    private void checkParentDirectoryExists() {
+        if (!path.getParent().exists()) {
+            try {
+                log.trace("Creating directory locally ${path.getParent().toAbsolutePath().toString()}")
+                Files.createDirectories(path.getParent())
+            } catch (IOException e) {
+                log.error("Couldn't create directory ${path.getParent().toAbsolutePath().toString()}", e)
+            }
+        }
+    }
+
     /**
      *
      * @return A path located in the original workdir
@@ -121,6 +132,20 @@ class LocalPath implements Path {
         }
     }
 
+    void setText( String text ){
+        setText( text, Charset.defaultCharset().toString() )
+    }
+
+    void setText( String text, String charset ){
+        final String absolutePath = path.toAbsolutePath().toString()
+        final def location = client.getFileLocation( absolutePath )
+        checkParentDirectoryExists()
+        log.trace("Write locally $absolutePath")
+        path.setText( text, charset )
+        def file = path.toFile()
+        client.addFileLocation( path.toString(), file.size(), file.lastModified(), location.locationWrapperID as long, true )
+    }
+
     byte[] getBytes(){
         final String absolutePath = path.toAbsolutePath().toString()
         final def location = getLocation( absolutePath )
@@ -134,6 +159,16 @@ class LocalPath implements Path {
                 return fileStream.getBytes()
             }
         }
+    }
+
+    void setBytes( byte[] bytes ){
+        final String absolutePath = path.toAbsolutePath().toString()
+        final def location = client.getFileLocation( absolutePath )
+        checkParentDirectoryExists()
+        log.trace("Write locally $absolutePath")
+        path.setBytes( bytes )
+        def file = path.toFile()
+        client.addFileLocation( path.toString(), file.size(), file.lastModified(), location.locationWrapperID as long, true )
     }
 
     Object withReader( Closure closure ){
@@ -153,6 +188,32 @@ class LocalPath implements Path {
                 return IOGroovyMethods.withReader(fileStream, closure)
             }
         }
+    }
+
+    def <T> T withWriter( Closure<T> closure ){
+        return withWriter( Charset.defaultCharset().toString(), closure )
+    }
+
+    def <T> T withWriter( String charset, Closure<T> closure ) {
+        final String absolutePath = path.toAbsolutePath().toString()
+        final def location = client.getFileLocation( absolutePath )
+        checkParentDirectoryExists()
+        log.trace("Write locally $absolutePath")
+        T rv = path.withWriter( charset, closure )
+        def file = path.toFile()
+        client.addFileLocation( path.toString(), file.size(), file.lastModified(), location.locationWrapperID as long, true )
+        return rv
+    }
+
+    def <T> T withPrintWriter( String charset, Closure<T> closure ){
+        final String absolutePath = path.toAbsolutePath().toString()
+        final def location = client.getFileLocation( absolutePath )
+        checkParentDirectoryExists()
+        log.trace("Write locally $absolutePath")
+        T rv = path.withPrintWriter( charset, closure )
+        def file = path.toFile()
+        client.addFileLocation( path.toString(), file.size(), file.lastModified(), location.locationWrapperID as long, true )
+        return rv
     }
 
     List<String> readLines(){
@@ -211,6 +272,44 @@ class LocalPath implements Path {
         }
     }
 
+    BufferedWriter newWriter(){
+        return newWriter( Charset.defaultCharset().toString(), false, false )
+    }
+
+    BufferedWriter newWriter( String charset ) {
+        return newWriter( charset, false, false )
+    }
+
+    BufferedWriter newWriter( boolean append ) {
+        return newWriter( Charset.defaultCharset().toString(), append, false )
+    }
+
+    BufferedWriter newWriter( String charset, boolean append ){
+        return newWriter( charset, append, false )
+    }
+
+    BufferedWriter newWriter( String charset, boolean append, boolean writeBom ){
+        final String absolutePath = path.toAbsolutePath().toString()
+        final def location = client.getFileLocation( absolutePath )
+        checkParentDirectoryExists()
+        log.trace("Write locally $absolutePath")
+        BufferedWriter bufferedWriter = path.newWriter( charset, append, writeBom )
+        def file = path.toFile()
+        client.addFileLocation( path.toString(), file.size(), file.lastModified(), location.locationWrapperID as long, true )
+        return bufferedWriter
+    }
+
+    PrintWriter newPrintWriter( String charset ){
+        final String absolutePath = path.toAbsolutePath().toString()
+        final def location = client.getFileLocation( absolutePath )
+        checkParentDirectoryExists()
+        log.trace("Write locally $absolutePath")
+        PrintWriter printWriter = path.newPrintWriter( charset )
+        def file = path.toFile()
+        client.addFileLocation( path.toString(), file.size(), file.lastModified(), location.locationWrapperID as long, true )
+        return printWriter
+    }
+
     public <T> T eachByte( Closure<T> closure ) throws IOException {
         final String absolutePath = path.toAbsolutePath().toString()
         final def location = getLocation( absolutePath )
@@ -255,6 +354,17 @@ class LocalPath implements Path {
         }
     }
 
+    def <T> T withOutputStream( Closure<T> closure ){
+        final String absolutePath = path.toAbsolutePath().toString()
+        final def location = client.getFileLocation( absolutePath )
+        checkParentDirectoryExists()
+        log.trace("Write locally $absolutePath")
+        T rv = path.withOutputStream( closure )
+        def file = path.toFile()
+        client.addFileLocation( path.toString(), file.size(), file.lastModified(), location.locationWrapperID as long, true )
+        return rv
+    }
+
     public BufferedInputStream newInputStream() throws IOException {
         final String absolutePath = path.toAbsolutePath().toString()
         final def location = getLocation( absolutePath )
@@ -267,6 +377,78 @@ class LocalPath implements Path {
             log.trace("Read remote $absolutePath")
             return new BufferedInputStream( fileStream )
         }
+    }
+
+    BufferedOutputStream newOutputStream(){
+        final String absolutePath = path.toAbsolutePath().toString()
+        final def location = client.getFileLocation( absolutePath )
+        checkParentDirectoryExists()
+        log.trace("Write locally $absolutePath")
+        BufferedOutputStream bufferedOutputStream = path.newOutputStream()
+        def file = path.toFile()
+        client.addFileLocation( path.toString(), file.size(), file.lastModified(), location.locationWrapperID as long, true )
+        return bufferedOutputStream
+    }
+
+    void write( String text ){
+        write(text, Charset.defaultCharset().toString(), false)
+    }
+
+    void write( String text, String charset ){
+        write(text, charset, false)
+    }
+
+    void write( String text, boolean writeBom ){
+        write(text, Charset.defaultCharset().toString(), writeBom)
+    }
+
+    void write( String text, String charset, boolean writeBom ){
+        final String absolutePath = path.toAbsolutePath().toString()
+        final def location = client.getFileLocation( absolutePath )
+        checkParentDirectoryExists()
+        log.trace("Write locally $absolutePath")
+        path.write( text, charset, writeBom )
+        def file = path.toFile()
+        client.addFileLocation( path.toString(), file.size(), file.lastModified(), location.locationWrapperID as long, true )
+        def loc = client.getFileLocation( absolutePath )
+    }
+
+    void leftShift(Object text) {
+        append(text)
+    }
+
+    void append( Object text ){
+        append(text, Charset.defaultCharset().toString(), false)
+    }
+
+    void append( Object text, String charset ){
+        append(text, charset, false)
+    }
+
+    void append( Object text, boolean writeBom ){
+        append(text, Charset.defaultCharset().toString(), writeBom)
+    }
+
+    void append( Object text, String charset, boolean writeBom ){
+        if ( !text ) {
+            return
+        }
+        final String absolutePath = path.toAbsolutePath().toString()
+        final def location = client.getFileLocation( absolutePath )
+        if ( wasDownloaded || location.sameAsEngine ) {
+            log.info("Append locally $absolutePath")
+            path.append( text, charset, writeBom )
+        } else {
+            try (FtpClient ftpClient = getConnection(location.node.toString(), location.daemon.toString())) {
+                byte[] bytes = text.toString().getBytes( charset )
+                InputStream stream = new ByteArrayInputStream( bytes )
+                log.info("Append remote $absolutePath")
+                ftpClient.appendFile( absolutePath, stream )
+            }
+        }
+        def file = path.toFile()
+        client.addFileLocation( path.toString(), file.size(), file.lastModified(), location.locationWrapperID as long, true )
+        def loc = client.getFileLocation( absolutePath )
     }
 
     private Map download(){
