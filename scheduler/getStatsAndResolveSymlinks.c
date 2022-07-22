e0a1222759f19d01dff024fdc656dc7ce9365073
Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>
2022-07-22 11:33:57 +0200

    Updated getStatsAndResolveSymlinks
    Signed-off-by: Lehmann_Fabian <fabian.lehmann@informatik.hu-berlin.de>

--- scheduler/getStatsAndResolveSymlinks.c
+++ scheduler/getStatsAndResolveSymlinks.c
@@ -10,7 +10,12 @@
 #define FULL_DESCR 0
 #define SHORT_DESCR_WITH_TIMESTAMP 1
 
+#define INFILES_NAME "infiles"
+#define OUTFILES_NAME "outfiles"
+
 #define STACK_MIN_SIZE 1
+
+#define SYMLINK_TARGET_BUFFER_SIZE 500
 
 struct symlink {
     char * src;
@@ -22,19 +27,13 @@ struct stack {
     int size;
     struct symlink * items;
 };
+
 struct stack * newStack(int size) {
     struct stack * ptr = (struct stack *) malloc(sizeof(struct stack *));
     ptr->top = -1;
     ptr->items = (struct symlink *) malloc(sizeof(struct symlink) * size);
     ptr->size = size;
     return ptr;
-}
-
-void deleteStack(struct stack * ptr) {
-    ptr->top = -1;
-    ptr->size = -1;
-    free(ptr->items);
-    free(ptr);
 }
 
 int isEmpty(struct stack * ptr) {
@@ -61,7 +60,7 @@ struct stack * push_symlink(struct stack * ptr, char * const src, char * const d
 
 const struct symlink * const head(struct stack * ptr) {
     if (isEmpty(ptr)) {
-        printf("Stack is empty!");
+        fprintf(stderr, "Error: Stack is empty!");
         exit(EXIT_FAILURE);
     }
     return &(ptr->items[ptr->top]);
@@ -69,10 +68,21 @@ const struct symlink * const head(struct stack * ptr) {
 
 void delete_top(struct stack * ptr) {
     if (isEmpty(ptr)) {
-        printf("Stack is empty!");
+        fprintf(stderr, "Error: Stack is empty!");
         exit(EXIT_FAILURE);
     }
+    free(ptr->items[ptr->top].src);
+    free(ptr->items[ptr->top].dst);
     ptr->top--;
+}
+
+void deleteStack(struct stack * ptr) {
+    while (!isEmpty(ptr)) {
+        delete_top(ptr);
+    }
+    ptr->size = -1;
+    free(ptr->items);
+    free(ptr);
 }
 
 int collectFileInformation(char * const * dir_to_search, const int version,
@@ -87,19 +97,38 @@ int getShortDescrAndTimestamp(char * const * dir,
 
 
 int main(int arc, char * const argv[]) {
+
     if (arc < 5) {
+        fprintf(stderr, "Error: too few arguments!\n");
         return -1;
     }
     argv++;
-    if (strcmp(argv[0], "short") != 0 && strcmp(argv[0], "long") != 0) {
-        printf("Error: version must be 'short' or 'long'\n");
+    struct timespec start_time, end_time;
+    int gettimeofday_rc;
+    if ((gettimeofday_rc = clock_gettime(CLOCK_REALTIME, &start_time)) != 0) {
+        fprintf(stderr, "Error getting the time of day\n");
+        return gettimeofday_rc;
+    }
+
+    if (strcmp(argv[0], INFILES_NAME) != 0 && strcmp(argv[0], OUTFILES_NAME) != 0) {
+        fprintf(stderr, "Error: version must be '%s' or '%s'\n", INFILES_NAME, OUTFILES_NAME);
         return -1;
     }
-    const int version = strcmp(argv++[0], "short") == 0
+    const char * const name = argv++[0];
+    const int version = strcmp(name, INFILES_NAME) == 0
         ? SHORT_DESCR_WITH_TIMESTAMP : FULL_DESCR;
     const char * const result_filename = argv++[0];
     const char * const local_dir = argv++[0];
     int rc = collectFileInformation(argv, version, local_dir, result_filename);
+
+    if ((gettimeofday_rc = clock_gettime(CLOCK_REALTIME, &end_time)) != 0) {
+        fprintf(stderr, "Error getting the time of day\n");
+        return gettimeofday_rc;
+    }
+    long long int start = start_time.tv_sec * 1000000000 + start_time.tv_nsec;
+    long long int end = end_time.tv_sec * 1000000000 + end_time.tv_nsec;
+    printf("%lli\n", (end - start) / 1000000);
+
     return rc;
 }
 
@@ -111,7 +140,7 @@ int collectFileInformation(char * const * dir_to_search, const int version,
     if (dir_ptr) {
         closedir(dir_ptr);
     } else {
-        printf("Error: the local directory '%s' does not exist.\n", local_dir);
+        fprintf(stderr, "Error: the local directory '%s' does not exist.\n", local_dir);
         return -1;
     }
 
@@ -119,18 +148,13 @@ int collectFileInformation(char * const * dir_to_search, const int version,
     if (dir_ptr) {
         closedir(dir_ptr);
     } else {
-        printf("Error: the directory to search '%s' does not exist.\n", dir_to_search[0]);
-        return -1;
-    }
-
-    if (strncmp(local_dir, dir_to_search[0], strlen(local_dir)) != 0) {
-        printf("Error: the directory to search is not located in the local directory\n");
+        fprintf(stderr, "Error: the directory to search '%s' does not exist.\n", dir_to_search[0]);
         return -1;
     }
 
     FILE * file_ptr = fopen(result_filename, "w");
     if (file_ptr == NULL) {
-        printf("Error opening the file %s\n", result_filename);
+        fprintf(stderr, "Error opening the file %s\n", result_filename);
         return -1;
     }
     int rc;
@@ -158,7 +182,7 @@ int getFullDescr(char * const * dir,
     int fts_options = FTS_PHYSICAL;
 
     if ((fts_ptr = fts_open(dir, fts_options, NULL)) == NULL) {
-        printf("Error traversing the directory %s\n", dir[0]);
+        fprintf(stderr, "Error traversing the directory %s\n", dir[0]);
         return -1;
     }
     ch_ptr = fts_children(fts_ptr, 0);
@@ -167,7 +191,7 @@ int getFullDescr(char * const * dir,
     }
     int skip_next = 0;
     struct stack * symlink_stack = newStack(STACK_MIN_SIZE);
-    char * symlink_target_path = (char *) malloc(500);
+    char * symlink_target_path = (char *) malloc(SYMLINK_TARGET_BUFFER_SIZE);
     while ((ptr = fts_read(fts_ptr)) != NULL) {
         if (ptr->fts_info == FTS_DP) {
             continue;
@@ -195,13 +219,43 @@ int getFullDescr(char * const * dir,
                     strcpy(symlink_target_path, "");
                 }
                 exists = 1 + access(symlink_target_path, F_OK); // test for file existence
+                if (!exists) {
+                    break;
+                }
+                // checking whether it is a directory:
+                struct stat target_file_stat;
+                int stat_rv;
+                if ((stat_rv = stat(symlink_target_path, &target_file_stat)) != 0) {
+                    fprintf(stderr, "Error reading the file %s\n", symlink_target_path);
+                    return stat_rv;
+                }
+                if (S_ISDIR(target_file_stat.st_mode)) {
+                    // if target is not local, we skip it
+                    if (strncmp(symlink_target_path, local_dir, strlen(local_dir)) != 0) {
+                        file_type = "non-local directory";
+                        break;
+                    }
+                    file_type = "directory";
+                    // if target is within the directory we are searching, we skip it,
+                    // to prevent searching a directory more than once
+                    if (strncmp(symlink_target_path, dir[0], strlen(dir[0])) == 0) {
+                        break;
+                    }
+                    fts_set(fts_ptr, ptr, FTS_FOLLOW);
+                    symlink_stack = push_symlink(symlink_stack, ptr->fts_path, symlink_target_path);
+                    skip_next = 1;
+                } else if (S_ISREG(target_file_stat.st_mode)) {
+                    file_type = "regular file";
+                }
                 break;
 
             default:
+                file_type = "unknown";
+                exists = 1;
                 break;
         }
         if (!isEmpty(symlink_stack) && file_type != "symbolic link") {
-            while(strncmp(head(symlink_stack)->src, ptr->fts_path, strlen(head(symlink_stack)->src)) != 0) {
+            while (strncmp(head(symlink_stack)->src, ptr->fts_path, strlen(head(symlink_stack)->src)) != 0) {
                 delete_top(symlink_stack);
                 if (isEmpty(symlink_stack)) {
                     break;
@@ -230,30 +284,6 @@ int getFullDescr(char * const * dir,
             ptr->fts_statp->st_atim.tv_sec, ptr->fts_statp->st_atim.tv_nsec,
             ptr->fts_statp->st_mtim.tv_sec, ptr->fts_statp->st_mtim.tv_nsec
         );
-        // if a symlink points to a directory, we continue searching that directory
-        if (file_type == "symbolic link") {
-            // if target is not local, we skip it
-            if (strncmp(symlink_target_path, local_dir, strlen(local_dir)) != 0) {
-                continue;
-            }
-            // if target is within the directory we are searching, we skip it,
-            // to prevent searching a directory more than once
-            if (strncmp(symlink_target_path, dir[0], strlen(dir[0])) == 0) {
-                continue;
-            }
-            // checking whether it is a directory:
-            struct stat target_file_stat;
-            int stat_rv;
-            if ((stat_rv = stat(symlink_target_path, &target_file_stat)) != 0) {
-                printf("Error reading the file %s\n", symlink_target_path);
-                return stat_rv;
-            }
-            if (S_ISDIR(target_file_stat.st_mode)) {
-                fts_set(fts_ptr, ptr, FTS_FOLLOW);
-                symlink_stack = push_symlink(symlink_stack, ptr->fts_path, symlink_target_path);
-                skip_next = 1;
-            }
-        }
     }
     free(symlink_target_path);
     deleteStack(symlink_stack);
@@ -268,19 +298,24 @@ int getShortDescrAndTimestamp(char * const * dir,
     struct timespec time_now;
     int gettimeofday_rc;
     if ((gettimeofday_rc = clock_gettime(CLOCK_REALTIME, &time_now)) != 0) {
-        printf("Error getting the time of day\n");
+        fprintf(stderr, "Error getting the time of day\n");
         return gettimeofday_rc;
     } 
     fprintf(file_ptr, "%li%li\n", time_now.tv_sec, time_now.tv_nsec);
 
     fprintf(file_ptr, "%s\n", dir[0]);
 
+    int prefix_len = strlen(dir[0]);
+    if (dir[0][prefix_len-1] != '/') {
+        prefix_len++;
+    }
+
     FTS * fts_ptr;
     FTSENT * ptr, * ch_ptr;
     int fts_options = FTS_PHYSICAL;
 
     if ((fts_ptr = fts_open(dir, fts_options, NULL)) == NULL) {
-        printf("Error traversing the directory %s\n", dir[0]);
+        fprintf(stderr, "Error traversing the directory %s\n", dir[0]);
         return -1;
     }
     ch_ptr = fts_children(fts_ptr, 0);
@@ -289,9 +324,12 @@ int getShortDescrAndTimestamp(char * const * dir,
     }
     int skip_next = 0;
     struct stack * symlink_stack = newStack(STACK_MIN_SIZE);
-    char * symlink_target_path = (char *) malloc(500);
+    char * symlink_target_path = (char *) malloc(SYMLINK_TARGET_BUFFER_SIZE);
     while ((ptr = fts_read(fts_ptr)) != NULL) {
         if (ptr->fts_info == FTS_DP) {
+            continue;
+        }
+        if (strncmp(ptr->fts_path, dir[0], strlen(ptr->fts_path)) == 0) {
             continue;
         }
         if (skip_next) {
@@ -312,18 +350,50 @@ int getShortDescrAndTimestamp(char * const * dir,
                 break;
             case FTS_SL:
                 file_type = "symbolic link";
-                realpath(ptr->fts_path, symlink_target_path);
+                int bytes_written = readlink(ptr->fts_path, symlink_target_path, SYMLINK_TARGET_BUFFER_SIZE);
+                symlink_target_path[bytes_written] = '\0';
+                // realpath(ptr->fts_path, symlink_target_path);
                 if (symlink_target_path == NULL) {
                     strcpy(symlink_target_path, "");
                 }
                 exists = 1 + access(symlink_target_path, F_OK); // test for file existence
+                if (!exists) {
+                    break;
+                }
+                // checking whether it is a directory:
+                struct stat target_file_stat;
+                int stat_rv;
+                if ((stat_rv = stat(symlink_target_path, &target_file_stat)) != 0) {
+                    fprintf(stderr, "Error reading the file %s\n", symlink_target_path);
+                    return stat_rv;
+                }
+                if (S_ISDIR(target_file_stat.st_mode)) {
+                    // if target is not local, we skip it
+                    if (strncmp(symlink_target_path, local_dir, strlen(local_dir)) != 0) {
+                        file_type = "non-local directory";
+                        break;
+                    }
+                    file_type = "directory";
+                    // if target is within the directory we are searching, we skip it,
+                    // to prevent searching a directory more than once
+                    if (strncmp(symlink_target_path, dir[0], strlen(dir[0])) == 0) {
+                        break;
+                    }
+                    fts_set(fts_ptr, ptr, FTS_FOLLOW);
+                    symlink_stack = push_symlink(symlink_stack, ptr->fts_path, symlink_target_path);
+                    skip_next = 1;
+                } else if (S_ISREG(target_file_stat.st_mode)) {
+                    file_type = "regular file";
+                }
                 break;
 
             default:
+                file_type = "unknown";
+                exists = 1;
                 break;
         }
         if (!isEmpty(symlink_stack) && file_type != "symbolic link") {
-            while(strncmp(head(symlink_stack)->src, ptr->fts_path, strlen(head(symlink_stack)->src)) != 0) {
+            while (strncmp(head(symlink_stack)->src, ptr->fts_path, strlen(head(symlink_stack)->src)) != 0) {
                 delete_top(symlink_stack);
                 if (isEmpty(symlink_stack)) {
                     break;
@@ -341,41 +411,12 @@ int getShortDescrAndTimestamp(char * const * dir,
         }
         fprintf(
             file_ptr,
-            "%s;%i;%s;%li;%s;%li%li;%li%li;%li%li\n",
-            ptr->fts_path,
+            "%s;%i;%s;%s\n",
+            ptr->fts_path + prefix_len,
             exists,
             symlink_target_path,
-            ptr->fts_statp->st_size,
-            file_type,
-            ptr->fts_statp->st_ctim.tv_sec, ptr->fts_statp->st_ctim.tv_nsec,
-            // ctim - time of last status change which is used as an approximation of the creation time
-            ptr->fts_statp->st_atim.tv_sec, ptr->fts_statp->st_atim.tv_nsec,
-            ptr->fts_statp->st_mtim.tv_sec, ptr->fts_statp->st_mtim.tv_nsec
+            file_type
         );
-        // if a symlink points to a directory, we continue searching that directory
-        if (file_type == "symbolic link") {
-            // if target is not local, we skip it
-            if (strncmp(symlink_target_path, local_dir, strlen(local_dir)) != 0) {
-                continue;
-            }
-            // if target is within the directory we are searching, we skip it,
-            // to prevent searching a directory more than once
-            if (strncmp(symlink_target_path, dir[0], strlen(dir[0])) == 0) {
-                continue;
-            }
-            // checking whether it is a directory:
-            struct stat target_file_stat;
-            int stat_rv;
-            if ((stat_rv = stat(symlink_target_path, &target_file_stat)) != 0) {
-                printf("Error reading the file %s\n", symlink_target_path);
-                return stat_rv;
-            }
-            if (S_ISDIR(target_file_stat.st_mode)) {
-                fts_set(fts_ptr, ptr, FTS_FOLLOW);
-                symlink_stack = push_symlink(symlink_stack, ptr->fts_path, symlink_target_path);
-                skip_next = 1;
-            }
-        }
     }
     free(symlink_target_path);
     deleteStack(symlink_stack);
